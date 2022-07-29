00dd51dc634e0be4e3c3bb8f8ae998984ab9eb69
Rafael Santana <rafaelsantana.devweb@gmail.com>
2022-07-28 21:48:16 -0300

    Create and styling todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Form } from './Components/Form';
+import { Circle, Trash } from 'phosphor-react';
+
+import { NewTodoForm } from './Components/NewTodoForm';
 import { Header } from './Components/Header';
 
 import styles from './App.module.css';
@@ -11,7 +13,40 @@ export function App() {
       <Header />
 
       <div className={styles.wrapper}>
-        <Form />
+        <NewTodoForm />
+
+        <main className={styles.main}>
+          <header>
+            <div>Tarefas criadas <span>5</span></div>
+            <div>Concluídas <span>2 de 5</span></div>            
+          </header>
+
+          <ul>
+            <li>
+              <button type='button'>
+                <Circle size={20}/>
+              </button>
+
+              <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+
+              <button type='button'>
+              <Trash size={20}/>
+              </button>
+            </li>
+
+            <li>
+              <button type='button'>
+                <Circle size={20}/>
+              </button>
+
+              <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+              
+              <button type='button'>
+              <Trash size={20}/>
+              </button>
+            </li>
+          </ul>
+        </main>
       </div>
     </>
   )
--- src/Components/NewTodoForm.tsx
+++ src/Components/NewTodoForm.tsx
@@ -1,8 +1,8 @@
 import { PlusCircle } from 'phosphor-react';
 
-import styles from './Form.module.css';
+import styles from './NewTodoForm.module.css';
 
-export function Form() {
+export function NewTodoForm() {
   return (
     <form className={styles.form}>
       <input placeholder='Adicione uma nova tarefa' type="text" />
@@ -10,5 +10,5 @@ export function Form() {
         Criar
         <PlusCircle size={16} /></button>
     </form>
-  )
+  );
 }
